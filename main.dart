66d9930a55d1923cdd0503d9df44ad0d884edf9a
Josuan Leonardo Hulom <hulomjosuan@gmail.com>
2025-05-31 15:34:09 +0800

    add constructor

--- main.dart
+++ main.dart
@@ -1,4 +1,14 @@
 class TestClass {
+  String? name;
+  int? age;
+
+  TestClass({this.name, this.age});
+
+  @override
+  String toString() {
+    return 'TestClass(name: $name, age: $age)';
+  }
+
   void displayMessage() {
     print('Hello from TestClass!');
   }
